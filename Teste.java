89529ce4f3db4b54944b5684a06c20ad21ec767e
thaise <thaisefloripa@gmail.com>
2011-11-15 20:38:34 -0200

    Programa simples para calcular média utilizando interface gráfica com a classe JOptionPane.

---
+++ Teste.java
@@ -0,0 +1,38 @@
+/**
+*Teste de programa com JOptionPane
+*/
+import javax.swing.JOptionPane;
+public class Teste{
+	
+	public static void main(String[] args){
+		String notas = JOptionPane.showInputDialog(null, "Digite suas notas: ", "Notas", JOptionPane.PLAIN_MESSAGE);
+		String[] notasSeparadas = notas.split(",");
+		
+		double[] notasParaMedia = new double[notasSeparadas.length];
+		for(int i = 0; i < notasSeparadas.length; i++){
+			notasParaMedia[i] = Double.parseDouble(notasSeparadas[i]);
+		}
+		double media = calcularMedia(notasParaMedia);
+		String situacao = avaliarSituacao(media);
+		JOptionPane.showMessageDialog(null, "Sua média é: "+media+" - Você está "+situacao);
+	}
+	
+	public static double calcularMedia(double[] notasParaMedia){
+		double media = 0;
+		for(int i = 0; i < notasParaMedia.length; i++){
+			media = media + notasParaMedia[i];
+		}
+		return media / notasParaMedia.length;
+	}
+	
+	public static String avaliarSituacao(double media){
+		String situacao = "";
+		if(media < 7){
+			situacao = "reprovado(a).";
+		}else{
+			situacao = "aprovado(a).";
+		}
+		
+		return situacao;
+	}
+}
